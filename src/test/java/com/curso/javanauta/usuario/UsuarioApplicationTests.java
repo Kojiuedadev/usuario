ef631c5a2f54a3da0f9d22de7df4c847e2bad805
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2025-08-26 21:12:27 -0300

    Commit inicial da minha aplicação

---
+++ src/test/java/com/curso/javanauta/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.javanauta.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
