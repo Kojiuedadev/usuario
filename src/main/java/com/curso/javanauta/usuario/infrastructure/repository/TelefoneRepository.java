13d65b3fd9c241d5de6a2b04371fc21672d03ac5
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2025-08-26 21:50:33 -0300

    Adicionado classes security

---
+++ src/main/java/com/curso/javanauta/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,11 @@
+package com.curso.javanauta.usuario.infrastructure.repository;
+
+
+import com.curso.javanauta.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+
+}
